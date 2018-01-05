3ec5a0a6ecd85196e1ff105633b84294e0982c24
nam.tt <nam.tt@neo-lab.vn>
2018-01-05 14:05:07 +0700

    struct folder and add config editor

---
+++ src/components/d3/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class D3 extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>D3</h1>
+      </React.Fragment>
+    )
+  }
+}
+
+export default D3
